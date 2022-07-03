21ab10e97e996f8643ebe5645843c8dd48e2c1d4
UserM4P <julianestebanparada@gmail.com>
2022-07-03 03:42:36 -0500

    Angular GifsApp

--- src/portfolio.js
+++ src/portfolio.js
@@ -23,10 +23,10 @@ const greeting = {
   username: "Julian Parada",
   title: "Hola, Mi nombre es Julian Esteban Parada",
   subTitle: emoji(
-    "Un apasionado Full Stack Software Developer, 🚀 tengo experiencia construyendo aplicaciones Web y Mobiles con JavaScript / Reactjs / Nodejs  y algunas otras geniales bibliotecas y frameworks.",
+    "Un apasionado Full Stack Software Developer, 🚀 tengo experiencia construyendo aplicaciones Web y Mobiles con JavaScript / Reactjs / Angular / Nodejs  y algunas otras geniales bibliotecas y frameworks.",
   ),
   resumeLink:
-    "https://drive.google.com/file/d/19vV460sAsv7EROumpyY2aAOKZlTW_3kW/view?usp=sharing", // Set to empty to hide the button
+    "https://drive.google.com/file/d/1OwK7DDOw9UzDT_hyNxTK6Nkq-oUcSkP7/view?usp=sharing", // Set to empty to hide the button
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
@@ -109,6 +109,10 @@ https://fontawesome.com/icons?d=gallery */
       skillName: "firebase",
       fontAwesomeClassname: "fas fa-fire"
     },
+    {
+      skillName: "Angular",
+      fontAwesomeClassname: "fab fa-angular"
+    }
     
   ],
   display: true // Set false to hide this section, defaults to true
@@ -156,15 +160,15 @@ const techStack = {
   experience: [
     {
       Stack: "Frontend/Design", //Insert stack or technology you have experience in
-      progressPercentage: "90%" //Insert relative proficiency in percentage
+      progressPercentage: "80%" //Insert relative proficiency in percentage
     },
     {
       Stack: "Backend",
-      progressPercentage: "50%"
+      progressPercentage: "80%"
     },
     {
       Stack: "Programming",
-      progressPercentage: "60%"
+      progressPercentage: "70%"
     }
   ],
   displayCodersrank: false // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
@@ -175,35 +179,29 @@ const techStack = {
 
 export const images = {
   img : [
-      {   
-          src : "https://i.imgur.com/nT2Nwg1.png",
-          href : "https://blais3pasc4l.github.io/salon_app/",
-          title : "Salon App",
-          description: "Aplicacion web para gestionar salones de belleza haciendo uso unicamente de html y css nativos"
-      },
-      {   
-          src : "https://i.imgur.com/lzeHH6L.png",
-          href : "https://blog-caf.netlify.app//",
-          title : "Blog App",
-          description: "Blog enfocado en demostrar mis conocimientos en html y css nativos"
-      },
-      {   
-        src : "https://i.imgur.com/yxYerji.png",
-        href : "https://blais3pasc4l.github.io/Edm-y-Rock/",
-        title : "Rock and EDM",
-        description: "Pagina web para promocionar un festival de rock y edm desarrollada con el objetivo de poner en practica mis conocimientos en UX/UI"
-      },
-      {   
-          src : "https://i.imgur.com/K93Xanj.png",
-          href : "https://userm4p.github.io/React-Buscador-de-gifs/",
-          title : "Buscador de gifs" , 
-          description: "Buscador de gifs hecho con React haciendo peticiones a una API de tipo REST que provee GIPHY, una de las más populares y que usan muchas redes sociales como WhatsApp, Facebook, Instagram, etc."
+      {
+        src: "https://i.imgur.com/QuOY00x.png",
+        href: "https://clock-react-app.netlify.app",
+        title: "Clock App",
+        description: "Es una aplicación web completamente responsive que nos permite visualizar un reloj en base a la relacion de aspecto que tenga tu pantalla, siendo que en el caso de que la resolucion sea mayor o menor a 1024px el reloj cambiara completamente de aspecto. Fue desarrollada usando React con typescript y CSS"
       },
       {
         src : "https://i.imgur.com/zUIKd3r.png",
         href : "https://github.com/Userm4p/HeroesApp",
         title : "Heroes App",
         description: "Es un landing page en donde se podrá encontrar una lista de los superheroes más conocidos de las franquicias de Marvel y DC comics, en donde haciendo click en cada una de sus respectivas tarjetas se puede acceder a una información más detallada."
+      },
+      {   
+        src : "https://i.imgur.com/HC59HXp.png",
+        href : "https://angular-julian-gifs-app.netlify.app",
+        title : "Buscador de gifs - Angular" , 
+        description: "Buscador de gifs hecho con Angular haciendo peticiones a una API de tipo REST que provee GIPHY, una de las más populares y que usan muchas redes sociales como WhatsApp, Facebook, Instagram, etc."
+      },
+      {   
+        src : "https://i.imgur.com/K93Xanj.png",
+        href : "https://userm4p.github.io/React-Buscador-de-gifs/",
+        title : "Buscador de gifs - React" , 
+        description: "Buscador de gifs hecho con React haciendo peticiones a una API de tipo REST que provee GIPHY, una de las más populares y que usan muchas redes sociales como WhatsApp, Facebook, Instagram, etc."
       },
       {
         src : "https://i.imgur.com/NqlgO2i.png",
@@ -223,12 +221,8 @@ export const images = {
         title: "Movies App",
         description: "Es una aplicación web que nos permite ver las peliculas mas populares y añadir peliculas a nuestros favoritos con sus respectivas imagenes, descripciones, comentarios de los usuarios y mas, la aplicación cuenta con un login completamente funcional que funciona en base al sistema de usuarios de The Movies DB. Fue diseñada con React, Redux, React-Router-Dom y CSS, la informacion de las peliculas fue extraida de una API REST"
       },
-      {
-        src: "https://i.imgur.com/QuOY00x.png",
-        href: "https://clock-react-app.netlify.app",
-        title: "Clock App",
-        description: "Es una aplicación web completamente responsive que nos permite visualizar un reloj en base a la relacion de aspecto que tenga tu pantalla, siendo que en el caso de que la resolucion sea mayor o menor a 1024px el reloj cambiara completamente de aspecto. Fue desarrollada usando React con typescript y CSS"
-      }
+      
+      
   ]
 }
 
@@ -254,13 +248,13 @@ const bigProjects = {
     {
       image: "https://i.imgur.com/ryrTRD2.png",
       projectName: "Parangón",
-      projectDesc: "Desarollor de React.js",
+      projectDesc: "Fullstack Developer",
     },
     {
       image:"https://media-exp1.licdn.com/dms/image/C4E0BAQGW-pd52Nihcw/company-logo_200_200/0/1568133506441?e=2147483647&v=beta&t=MFDfKsyHIcZ9veB_wOT15bEY7aK99MUzwDpurxuIrcw",
       projectName: "Png technologies solution",
-      projectDesc: "Desarollador Frontend",
-    }
+      projectDesc: "Fullstack Developer - Fullstack Semi Senior Developer",
+    },
   ],
   display: true 
 };
